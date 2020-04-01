b994f3853ac2075c0f3a1a27c85f565b91b7607e
Arne Schilling <arne_schilling@web.de>
2020-04-01 23:34:25 +0200

    Create database-based login capabilities and new java documents
    This creates a new class 'Database'. The login process is being modified
    and will validate the supplied credentials against the database.
    
    Also, the java documentation for every public method and class is
    updated.

--- src/window/UserLogin.java
+++ src/window/UserLogin.java
@@ -1,34 +1,36 @@
 package window;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
-import javax.swing.JPasswordField;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.JTabbedPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
 
+/**
+ * The primary window of the application. It provides login capabilities for the
+ * user prior to granting access to the application.
+ * 
+ * @author Arne, Max
+ *
+ */
+@SuppressWarnings("serial")
 public class UserLogin extends JFrame {
 
 	private JPanel contentPane;
 	private JPasswordField fieldPassword;
 	private JTextField fieldUsername;
 
-	public JPasswordField getFieldPassword() {
-		return fieldPassword;
-	}
-
-	public JTextField getFieldUsername() {
-		return fieldUsername;
-	}
-
 	/**
 	 * Launch the application.
+	 * 
+	 * @param args the arguments passed to the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -47,42 +49,79 @@ public class UserLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public UserLogin() {
-		// Globale Parameter des Frames
+		initialize();
+	}
+
+	private void initialize() {
+		initGlobalParameters();
+		initStaticLabels();
+		initButtons();
+		initFields();
+	}
+
+	private void initGlobalParameters() {
+		setTitle("Applikation X");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(2500, 100, 450, 300);
+		setBounds(1400, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		// Statische Labels		
+	}
+
+	private void initStaticLabels() {
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(170, 146, 144, 14);
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPassword.setBounds(183, 124, 68, 14);
 		contentPane.add(lblPassword);
-		
+
 		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(170, 71, 63, 14);
+		lblUsername.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsername.setBounds(183, 68, 68, 14);
 		contentPane.add(lblUsername);
-		
+
 		JLabel lblWelcome = new JLabel("Willkommen bei Applikation X");
-		lblWelcome.setBounds(125, 22, 155, 14);
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcome.setBounds(121, 22, 192, 14);
 		contentPane.add(lblWelcome);
-		
-		// Buttons und Textfelder
+	}
+
+	private void initButtons() {
 		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(157, 202, 89, 23);
+		btnLogin.setBounds(172, 213, 89, 23);
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				login();
+			}
+		});
 		contentPane.add(btnLogin);
-		// Meine Logik fuer den Button
-		ActionListener actionOnClick = new ActionOnLogin();
-		btnLogin.addActionListener(actionOnClick);
-		
+	}
+
+	private void initFields() {
 		fieldPassword = new JPasswordField();
-		fieldPassword.setBounds(125, 171, 144, 20);
+		fieldPassword.setBounds(145, 149, 144, 20);
 		contentPane.add(fieldPassword);
-		
+
 		fieldUsername = new JTextField();
-		fieldUsername.setBounds(125, 96, 144, 20);
+		fieldUsername.setBounds(145, 93, 144, 20);
 		contentPane.add(fieldUsername);
-		fieldUsername.setColumns(10);
+	}
+
+	private void login() {
+		if (isValidLogin()) {
+			System.out.println("Login erfolgreich"); // for DEBUG
+		} else {
+			System.out.println("Login fehlgeschlagen");// for DEBUG
+		}
+	}
+
+	private boolean isValidLogin() {
+		Database db = Database.db(); // Die Datenbank in der die Logindaten stehen
+
+		// Logindaten des Benutzers auslesen
+		String pw = new String(fieldPassword.getPassword());
+		String name = fieldUsername.getText();
+
+		return db.validateCredentials(name, pw);
 	}
 }
